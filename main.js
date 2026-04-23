8e0a7dbd34c2776f4cb5f7b4bb9bf72ba49e542b
Dafreeman794 <nategcook@gmail.com>
2026-04-22 17:33:04 -0700

    I got it working!
    changed the relative updating of the balls,, as well as changing a few names

--- main.js
+++ main.js
@@ -14,69 +14,72 @@ function random(min, max) {
   return num;
 }
 
-function Ball(x, y, velX, velY, color, size) {
-  this.x = y;
+function Elic(x, y, velX, velY, color, size) {
+  this.x = x;
   this.y = y;
   this.velX = velX;
   this.velY = velY;
   this.color = color;
   this.size = size;
+  this.image = new Image();
+  this.image.src = 'image/floating Elic.png';  // Path to your image
 }
 
-Ball.prototype.draw = function() {
-  ctx.beginPath();
-  ctx.fillStyle = this.color;
-  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-  ctx.fill();
 
+Elic.prototype.draw = function() {
+  ctx.drawImage(this.image, this.x - this.size, this.y - this.size, this.size * 2, this.size * 2);
 }
 
-Ball.prototype.update = function() {
-  if ((this.x + this.size) >=width) {
-    this.valX = -(this.velX);
+Elic.prototype.update = function() {
+  this.x += this.velX;
+  this.y += this.velY;
+
+  if (this.x + this.size >= width) {
+    this.x = width - this.size;
+    this.velX = -this.velX;
   }
 
-  if ((this.x - this.size) <= 0) {
-    this.velX = -(this.velX);
+  if (this.x - this.size <= 0) {
+    this.x = this.size;
+    this.velX = -this.velX;
   }
   
-  if ((this.y + this.size) >= height) {
-    this.velY + -(this.velY);
+  if (this.y + this.size >= height) {
+    this.y = height - this.size;
+    this.velY = -this.velY;
   }
 
-  if ((this.y - this.size) <= 0) {
-    this.velY = -(this.velY);
+  if (this.y - this.size <= 0) {
+    this.y = this.size;
+    this.velY = -this.velY;
   }
-
-  this.x +=this.velX;
-  this.y += this.velY;
 }
 
-let balls = [];
+let elics = [];
 
-while (balls.length < 25) {
+while (elics.length < 25) {
   let size = random(10, 20);
-  let ball = new Ball(
-    random(0 + size, width, - size),
-    random(0 + size, height, - size),
+  let ball = new Elic(
+    random(0 + size, width - size),
+    random(0 + size, height - size),
     random(-7,7),
     random(-7,7),
     'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')', 
     size
   );
 
-  balls.push(ball);
+  elics.push(ball);
 }
 
-Ball.prototype.collisionDetect = function() {
-  for (let j = 0; j < Ball.length; j++) {
-    if (!(this === balls[j])) {
-      const dx = this.x - balls[j].x;
-      const dy = this.y - balls[j].y;
+Elic.prototype.collisionDetect = function() {
+  for (let j = 0; j < Elic.length; j++) {
+    if (!(this === elics[j])) {
+      const dx = this.x - elics[j].x;
+      const dy = this.y - elics[j].y;
       const distance = Math.sqrt(dx * dx + dy * dy);
       
-      if (distance < this.size + balls[j].size) {
-        balls[j].color = this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')';
+      if (distance < this.size + elics[j].size) {
+        elics[j].color = this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')';
       }
     }
   }
@@ -86,10 +89,10 @@ function loop() {
   ctx.fillStyle = 'rgba(0, 0, 0, 0,25)';
   ctx.fillRect(0, 0, width, height);
 
-  for (let i = 0; i < balls.length; i++) {
-    balls[i].draw();
-    balls[i].update();
-    balls[i].collisionDetect();
+  for (let i = 0; i < elics.length; i++) {
+    elics[i].draw();
+    elics[i].update();
+    elics[i].collisionDetect();
   }
 
   requestAnimationFrame(loop);
